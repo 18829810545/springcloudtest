d18eda45f07625f823d098f44b9e59c225deed10
18829810545 <1429795441@qq.com>
2021-01-15 11:11:59 +0800

    redis test

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.data.redis.core.RedisTemplate;
 import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_Ok(@PathVariable("id")Integer id) {
@@ -34,5 +37,28 @@ public class PaymentController {
     public String paymentCircuitBreaker(@PathVariable("id")Integer id) {
         String result = paymentService.paymentCircuitBreaker(id);
         return result;
+    }
+
+
+    public static void main(String[] args) {
+//        Jedis jedis = new Jedis("127.0.0.1",6379);
+//        jedis.set("money","100");
+//        jedis.set("out","0");
+//        jedis.watch("money");
+//        Transaction transaction = jedis.multi();
+//        try {
+//            transaction.decrBy("money", 10l);
+//            transaction.incrBy("out", 10l);
+//            transaction.exec();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            jedis.unwatch();
+//        } finally {
+//            System.out.println(jedis.get("money"));
+//            System.out.println(jedis.get("out"));
+//            jedis.close();
+
+//        }
+
     }
 }
